e07c641ea55baef64414304cde104bc8097813b9
Felix Woestmann <felix.wostmann@edps.europa.eu>
2024-10-04 18:09:00 +0200

    Remove unused formidable lib

--- bin/website-evidence-collector-server.js
+++ bin/website-evidence-collector-server.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const formidable = require("express-formidable");
 const app = express();
 const port = 8080;
-
-app.use(formidable());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // update to match the domain you will make the request from
