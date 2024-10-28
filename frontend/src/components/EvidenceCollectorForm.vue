4f3c52928135ef9925b90162a56032021be0b760
Felix Woestmann <felix.wostmann@edps.europa.eu>
2024-10-28 18:13:47 +0100

    Make Form and HTML output independently scrollable

--- frontend/src/components/EvidenceCollectorForm.vue
+++ frontend/src/components/EvidenceCollectorForm.vue
@@ -1,141 +1,143 @@
 <!-- EvidenceCollectionForm.vue -->
 <template>
   <div class="flex flex-col md:flex-row">
-    <div class="w-full md:w-2/6 px-4 my-auto">
-      <Vueform :endpoint=false @submit=handleSubmit validate-on="step|change" :display-errors="true"
-               add-class="vf-evidence-collection-form">
-        <StaticElement name="title" tag="h1" content="Start Evidence Collection"/>
-        <TextElement
-            name="website_url"
-            :rules="[
-        'required',
-      ]"
-            input-type="url"
-            placeholder="http://example.com"
-        />
-        <StaticElement
-            name="divider"
-            tag="hr"
-        />
-        <StaticElement
-            name="max_option_label"
-            tag="h4"
-            content="Maximum Additional Links to browse"
-            align="left"
-            info="When larger than 0 links found on the site will be included in the collection up to the maximum defined here."
-            size="sm"
-        />
-        <TextElement
-            name="max_option_input"
-            input-type="number"
-            :rules="[
+    <div class="w-full md:w-2/6 pl-4" >
+      <div class="max-h-screen overflow-y-auto pr-2 py-3">
+        <Vueform :endpoint=false @submit=handleSubmit validate-on="step|change" :display-errors="true"
+                 add-class="vf-evidence-collection-form">
+          <StaticElement name="title" class="border-b-2 pb-3" tag="h1" content="Start Evidence Collection"/>
+          <TextElement
+              name="website_url"
+              :rules="[
+                'required',
+              ]"
+              input-type="url"
+              placeholder="http://example.com"
+          />
+          <StaticElement
+              name="divider"
+              tag="hr"
+          />
+          <StaticElement
+              name="max_option_label"
+              tag="h4"
+              content="Maximum Additional Links to browse"
+              align="left"
+              info="When larger than 0 links found on the site will be included in the collection up to the maximum defined here."
+              size="sm"
+          />
+          <TextElement
+              name="max_option_input"
+              input-type="number"
+              :rules="[
               'nullable',
               'min:0',
               'max:150',
               'integer',
       ]"
-            autocomplete="off"
-            placeholder="0"
-            default="0"
-            :floating="false"
-            info="The maximum number of links that will be browsed."
-        />
-        <StaticElement
-            name="sleep_option_label"
-            tag="h4"
-            content="Delay After Page Load (ms)"
-            align="left"
-        />
-        <TextElement
-            name="sleep_option_input"
-            input-type="number"
-            :rules="[
+              autocomplete="off"
+              placeholder="0"
+              default="0"
+              :floating="false"
+              info="The maximum number of links that will be browsed."
+          />
+          <StaticElement
+              name="sleep_option_label"
+              tag="h4"
+              content="Delay After Page Load (ms)"
+              align="left"
+          />
+          <TextElement
+              name="sleep_option_input"
+              input-type="number"
+              :rules="[
         'nullable',
         'min:0',
         'integer',
       ]"
-            autocomplete="off"
-            info="Amount of sleep in milliseconds after a page load"
-            placeholder="3000"
-            default="3000"
-            :floating="false"
-            :addons="{
+              autocomplete="off"
+              info="Amount of sleep in milliseconds after a page load"
+              placeholder="3000"
+              default="3000"
+              :floating="false"
+              :addons="{
         after: 'milliseconds',
       }"
-        />
-        <StaticElement
-            name="timeout_input_label"
-            tag="h4"
-            content="Page Load Timeout (ms)"
-            align="left"
-        />
-        <TextElement
-            name="timeout_input_option"
-            input-type="number"
-            :rules="[
+          />
+          <StaticElement
+              name="timeout_input_label"
+              tag="h4"
+              content="Page Load Timeout (ms)"
+              align="left"
+          />
+          <TextElement
+              name="timeout_input_option"
+              input-type="number"
+              :rules="[
         'nullable',
         'min:0',
         'integer',
       ]"
-            autocomplete="off"
-            info="Page load timeout in milliseconds "
-            placeholder="0"
-            :floating="false"
-            default="0"
-            :addons="{
+              autocomplete="off"
+              info="Page load timeout in milliseconds "
+              placeholder="0"
+              :floating="false"
+              default="0"
+              :addons="{
         after: 'milliseconds',
       }"
-            :attrs="{
+              :attrs="{
         'hide-spin-buttons': 'true',
       }"
-        />
-        <StaticElement
-            name="seed_option_label"
-            tag="h4"
-            content="Seed for deterministic link selection"
-        />
-        <TextElement
-            name="seed_option_input"
-            placeholder="jfmixuirofujq3po4utg90qutucngiuqgrpi"
-            autocomplete="off"
-            :floating="false"
-        />
-        <StaticElement
-            name="first_party_uri_option_label"
-            tag="h4"
-            content="List of URIs Considered First-Party (one per line)"
-        />
-        <TextareaElement
-            name="first_party_uri_option_input"
-            :floating="false"
-            placeholder="http://example.com/example;http://another-example.com/"
-            :autogrow="false"
-        />
-        <StaticElement
-            name="browse_link_option_label"
-            tag="h4"
-            content="Links to Include in collection (one per line)"
-            top="1"
-            align="left"
-        />
-        <TextareaElement
-            name="browse_link_option_input"
-            :autogrow="false"
-            :floating="false"
-            placeholder="http://example.com/example;http://example.com/another-example"
-        />
-        <StaticElement
-            name="divider_1"
-            tag="hr"
-        />
-        <ButtonElement
-            name="submit"
-            button-label="Start collection"
-            :submits="true"
-            :full="true"
-            align="center"
-        />
-      </Vueform>
+          />
+          <StaticElement
+              name="seed_option_label"
+              tag="h4"
+              content="Seed for deterministic link selection"
+          />
+          <TextElement
+              name="seed_option_input"
+              placeholder="jfmixuirofujq3po4utg90qutucngiuqgrpi"
+              autocomplete="off"
+              :floating="false"
+          />
+          <StaticElement
+              name="first_party_uri_option_label"
+              tag="h4"
+              content="List of URIs Considered First-Party (one per line)"
+          />
+          <TextareaElement
+              name="first_party_uri_option_input"
+              :floating="false"
+              placeholder="http://example.com/example;http://another-example.com/"
+              :autogrow="false"
+          />
+          <StaticElement
+              name="browse_link_option_label"
+              tag="h4"
+              content="Links to Include in collection (one per line)"
+              top="1"
+              align="left"
+          />
+          <TextareaElement
+              name="browse_link_option_input"
+              :autogrow="false"
+              :floating="false"
+              placeholder="http://example.com/example;http://example.com/another-example"
+          />
+          <StaticElement
+              name="divider_1"
+              tag="hr"
+          />
+          <ButtonElement
+              name="submit"
+              button-label="Start collection"
+              :submits="true"
+              :full="true"
+              align="center"
+          />
+        </Vueform>
+      </div>
     </div>
     <div class="inline-block h-screen min-h-[2em] w-0.5 self-stretch bg-neutral-100 dark:bg-white/10"></div>
     <div class="w-full md:w-4/6 relative">
@@ -151,22 +153,29 @@
                   @click="downloadHtml">Download HTML
           </button>
         </div>
-        <iframe :srcdoc="sanitizedHtml" ref="iframe" class="h-screen iframe-container pt-12"></iframe>
-      </div>
-        <div v-else class="flex items-center justify-center min-h-screen">
-          <div v-if="isRequestRunning==true" role="status" class="flex items-center">
-            <p class="text-black text-lg font-semibold mr-2">Loading...</p>
-            <svg aria-hidden="true" class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-eu-accent-140 " viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-           </svg>
-
-          </div>
-          <div v-else class="p-6 text-center">
-            <h1 class="text-2xl font-bold mb-4">Output will be displayed here</h1>
-            <p class="text-gray-600">Enter a URL and submit the form to start</p>
-          </div>
+        <div class="max-h-screen overflow-y-auto">
+          <iframe :srcdoc="sanitizedHtml" ref="iframe" class="h-screen iframe-container pt-12"></iframe>
         </div>
+      </div>
+      <div v-else class="flex items-center justify-center min-h-screen">
+        <div v-if="isRequestRunning==true" role="status" class="flex items-center">
+          <p class="text-black text-lg font-semibold mr-2">Loading...</p>
+          <svg aria-hidden="true" class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-eu-accent-140 "
+               viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"/>
+            <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"/>
+          </svg>
+
+        </div>
+        <div v-else class="p-6 text-center">
+          <h1 class="text-2xl font-bold mb-4">Output will be displayed here</h1>
+          <p class="text-gray-600">Enter a URL and submit the form to start</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -181,7 +190,7 @@ const isViteDevEnv = import.meta.env.DEV
 const sanitizedHtml = ref<string | null>(null);
 const pdfUrl = ref<string | null>(null);
 const htmlUrl = ref<string | null>(null)
-const isRequestRunning=ref<boolean>(false)
+const isRequestRunning = ref<boolean>(false)
 
 function reset() {
   pdfUrl.value = null;
@@ -208,7 +217,7 @@ async function handleSubmit(form$, _) {
   // Show loading spinner
   form$.submitting = true
   form$.cancelToken = form$.$vueform.services.axios.CancelToken.source()
-  isRequestRunning.value=true
+  isRequestRunning.value = true
 
   let response;
 
@@ -235,7 +244,7 @@ async function handleSubmit(form$, _) {
   } finally {
     // Hide loading spinner
     form$.submitting = false
-    isRequestRunning.value=false
+    isRequestRunning.value = false
   }
 }
 
